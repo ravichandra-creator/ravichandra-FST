01fc8e7280df621aec0b5c2b1762e47801d75d28
ravichandra-creator <62549176+ravichandra-creator@users.noreply.github.com>
2020-04-17 06:20:24 +0530

    Add files via upload
    for hacker rank test

---
+++ KnightMinMoves.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class KnightMinMoves {
+
+	// To calculate possible moves
+	static int findMinMoves(int n, int start_row, int star_col, int end_row, int end_col) {
+		// All possible moves of a knight
+		int X[] = { 2, 1, -1, -2, -2, -1, 1, 2 };
+		int Y[] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+
+		List<Integer> count = new ArrayList<>();
+
+		if (start_row < 0 && star_col < 0 && end_row > n && end_col > n) {
+			return -1;
+		}
+
+		// Check if each possible move is valid or not
+		for (int i = 0; i < 8; i++) {
+			// Position of knight after move
+			int c = 1;
+			int x = start_row;
+			int y = star_col;
+			x = x + X[i];
+			y = y + Y[i];
+			if (x == end_row && y == end_col) {
+				if (x >= 0 && y >= 0 && x < n && y < n) {
+					count.add(i);
+				}
+			}
+
+			for (int j = 0; j < 8; j++) {
+				// int cc=c;
+				int xx = x;
+				int yy = y;
+				xx = xx + X[j];
+				yy = yy + Y[j];
+				if (xx == end_row && yy == end_col) {
+					if (x >= 0 && y >= 0 && x < n && y < n) {
+						count.add(c + 1);
+						break;
+					}
+
+				}
+
+			}
+
+		}
+		Collections.sort(count);
+		int minmoves = -1;
+		if (count.size() >= 1) {
+			minmoves = count.get(0);
+		}
+		System.out.println(count);
+		return minmoves;
+	}
+
+	// Driver program to check findPossibleMoves()
+	public static void main(String[] args) {
+
+		System.out.println(findMinMoves(10, 4, 4, 4, 8));
+	}
+}
